340d29ceb265d454acecd98813d70aa38518d816
Connor Rowland <cjr6734@yahoo.com>
2015-06-28 21:57:37 -0400

    finished connect four

--- app.js
+++ app.js
@@ -266,80 +266,107 @@ var Game = {
 	},
 
 	winningCombinations: [
+       [a1, a2, a3, a4],
+       [a2, a3, a4, a5],
+       [a3, a4, a5, a6],
+       [a4, a5, a6, a7],
+       [b1, b2, b3, b4],
+       [b2, b3, b4, b5],
+       [b3, b4, b5, b6],
+       [b4, b5, b6, b7],
+       [c1, c2, c3, c4],
+       [c2, c3, c4, c5],
+       [c3, c4, c5, c6],
+       [c4, c5, c6, c7],
+       [d1, d2, d3, d4],
+       [d2, d3, d4, d5],
+       [d3, d4, d5, d6],
+       [d4, d5, d6, d7],
+       [e1, e2, e3, e4],
+       [e2, e3, e4, e5],
+       [e3, e4, e5, e6],
+       [e4, e5, e6, e7],
+       [f1, f2, f3, f4],
+       [f2, f3, f4, f5],
+       [f3, f4, f5, f6],
+       [f4, f5, f6, f7],
        [a1, b1, c1, d1],
-        [b1, c1, d1, e1],
-        [c1, d1, e1, f1],
-        [d1, e1, f1, g1],
-        [a2, b2, c2, d2],
-        [b2, c2, d2, e2],
-        [c2, d2, e2, f2],
-        [d2, e2, f2, g2],
-        [a3, b3, c3, d3],
-        [b3, c3, d3, e3],
-        [c3, d3, e3, f3],
-        [d3, e3, f3, g3],
-        [a4, b4, c4, d4],
-        [b4, c4, d4, e4],
-        [c4, d4, e4, f4],
-        [d4, e4, f4, g4],
-        [a5, b5, c5, d5],
-        [b5, c5, d5, e5],
-        [c5, d5, e5, f5],
-        [d5, e5, f5, g5],
-        [a6, b6, c6, d6],
-        [b6, c6, d6, e6],
-        [c6, d6, e6, f6],
-        [d6, e6, f6, g6],
-        [a1, a2, a3, a4],
-        [a2, a3, a4, a5],
-        [a3, a4, a5, a6],
-        [b1, b2, b3, b4],
-        [b2, b3, b4, b5],
-        [b3, b4, b5, b6],
-        [c1, c2, c3, c4],
-        [c2, c3, c4, c5],
-        [c3, c4, c5, c6],
-        [d1, d2, d3, d4],
-        [d2, d3, d4, d5],
-        [d3, d4, d5, d6],
-        [e1, e2, e3, e4],
-        [e2, e3, e4, e5],
-        [e3, e4, e5, e6],
-        [f1, f2, f3, f4],
-        [f2, f3, f4, f5],
-        [f3, f4, f5, f6],
-        [g1, g2, g3, g4],
-        [g2, g3, g4, g5],
-        [g3, g4, g5, g6],
-        [a1, b2, c3, d4],
-        [b1, c2, d3, e4],
-        [c1, d2, e3, f4],
-        [d1, e2, f3, g4],
-        [g1, f2, e3, d4],
-        [f1, e2, d3, c4],
-        [e1, d2, c3, b4],
-        [d1, c2, b3, a4],
-        [a2, b3, c4, d5],
-        [b2, c3, d4, e5],
-        [c2, d3, e4, f5],
-        [d2, e3, f4, g5],
-        [g2, f3, e4, d5],
-        [f2, e3, d4, c5],
-        [e2, d3, c4, b5],
-        [d2, c3, b4, a5],
-        [a3, b4, c5, d6],
-        [b3, c4, d5, e6],
-        [c3, d4, e5, f6],
-        [d3, e4, f5, g6],
-        [g3, f4, e5, d6],
-        [f3, e4, d5, c6],
-        [e3, d4, c5, b6],
-        [d3, c4, b5, a6]
+       [b1, c1, d1, e1],
+       [c1, d1, e1, f1],
+       [a2, b2, c2, d2],
+       [b2, c2, d2, e2],
+       [c2, d2, e2, f2],
+       [a3, b3, c3, d3],
+       [b3, c3, d3, e3],
+       [c3, d3, e3, f3],
+       [a4, b4, c4, d4],
+       [b4, c4, d4, e4],
+       [c4, d4, e4, f4],
+       [a5, b5, c5, d5],
+       [b5, c5, d5, e5],
+       [c5, d5, e5, f5],
+       [a6, b6, c6, d6],
+       [b6, c6, d6, e6],
+       [c6, d6, e6, f6],
+       [a7, b7, c7, d7],
+       [b7, c7, d7, e7],
+       [c7, d7, e7, f7],
+       [a1, b2, c3, d4],
+       [a2, b3, c4, d5],
+       [a3, b4, c5, d6],
+       [a4, b5, c6, d7],
+       [a4, b3, c2, d1],
+       [a5, b4, c3, d2],
+       [a6, b5, c4, d3],
+       [a7, b6, c5, d4],
+       [b1, c2, d3, e4],
+       [b2, c3, d4, e5],
+       [b3, c4, d5, e6],
+       [b4, c5, d6, e7],
+       [b4, c3, d2, e1],
+       [b5, c4, d3, e2],
+       [b6, c5, d4, e3],
+       [b7, c6, d5, e4],
+       [c1, d2, e3, f4],
+       [c2, d3, e4, f5],
+       [c3, d4, e5, f6],
+       [c4, d5, e6, f7],
+       [c4, d3, e2, f1],
+       [c5, d4, e3, f2],
+       [c6, d5, e4, f3],
+       [c7, d6, e5, f4],
    ],
-
-
+   getAWin: function(){
+   		for (var i = 0; i < Game.winningCombinations.length; i++) {
+			var blackCombinations = 0;
+			var redCombinations = 0;
+			for (var j = 0; j < Game.winningCombinations[i].length; j++) {
+				var boxId = Game.winningCombinations[i][j];
+				//var boxIdClass = $(".empty");
+				var boxTokenId = boxId.getAttribute("id")
+				if (Game[boxTokenId].children(".blackToken").length === 1 ){
+					blackCombinations++;
+				}else if(Game[boxTokenId].children(".redToken").length === 1 ){
+					redCombinations++;
+				};
+			};
+				if (blackCombinations == 4){
+					alert('Black team wins!');
+				}else if(redCombinations == 4){
+					alert('Red team wins!');
+				}
+		};
+	},
+	// changeTurnText: function(){
+	// 	if(Game.turn===0){
+	// 		$("#turns").text("Black Turn");
+	// 	}else if(Game.turn===1){
+	// 		$("#turns").text("Red Turn");
+	// 	}
+	// },
 
 };
+Game.getAWin();
 $('#button1').on("click",Game.dropOneToLowest);
 $('#button2').on("click",Game.dropTwoToLowest);
 $('#button3').on("click",Game.dropThreeToLowest);
@@ -348,33 +375,12 @@ $('#button5').on("click",Game.dropFiveToLowest);
 $('#button6').on("click",Game.dropSixToLowest);
 $('#button7').on("click",Game.dropSevenToLowest);
 
+$('#button1').on("click",Game.getAWin);
+$('#button2').on("click",Game.getAWin);
+$('#button3').on("click",Game.getAWin);
+$('#button4').on("click",Game.getAWin);
+$('#button5').on("click",Game.getAWin);
+$('#button6').on("click",Game.getAWin);
+$('#button7').on("click",Game.getAWin);
 
 
-// var changeButtonColor = function(){
-// 	var turn = 0; //turn 0 is black, turn 1 is red 
-// 	if(turn===0){
-// 		$('button1').attr("id","showBlackButton");
-// 		$('button2').attr("id","showBlackButton");
-// 		$('button3').attr("id","showBlackButton");
-// 		$('button4').attr("id","showBlackButton");
-// 		$('button5').attr("id","showBlackButton");
-// 		$('button6').attr("id","showBlackButton");
-// 		$('button7').attr("id","showBlackButton");
-// 	}else if(turn ===1){
-// 		$('button1').attr("id","showRedButton");
-// 		$('button2').attr("id","showRedButton");
-// 		$('button3').attr("id","showRedButton");
-// 		$('button4').attr("id","showRedButton");
-// 		$('button5').attr("id","showRedButton");
-// 		$('button6').attr("id","showRedButton");
-// 		$('button7').attr("id","showRedButton");
-
-// 	}
-// } 
-
-// $('button2').on("click",buttonAppear);
-// $('button3').on("click",buttonAppear);
-// $('button4').on("click",buttonAppear);
-// $('button5').on("click",buttonAppear);
-// $('button6').on("click",buttonAppear);
-// $('button7').on("click",buttonAppear);
